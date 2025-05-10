abf2e7c3a5d23c6defa7f0a43d7668e9ee9bd804
Leydi-Aro <leydiaro4@gmail.com>
2025-05-09 20:00:15 -0500

    feat:despliegue 3.0

--- src/app/shared/environments/environment.development.ts
+++ src/app/shared/environments/environment.development.ts
@@ -1,9 +1,5 @@
-export const environment= {
 
-  production:false,
-  //serverBasePath: 'http://localhost:8080/api/v1',
-  serverBasePath:'https//viks-adventures-api-hipx.onrender.com/api/v1',
-  //serverBasePath: 'https://viks-adventures-api-master.onrender.com/api/v1',
-  //serverBasePath: 'https://viks-adventures-api-production.up.railway.app/api/v1',
-
-}
+export const environment = {
+  production: false,
+  serverBasePath: 'https://viks-adventures-api-hipx.onrender.com/api/v1',
+};
